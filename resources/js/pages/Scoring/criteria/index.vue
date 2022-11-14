a9c870a6d21dadba48183e31cf14383bea8316bb
ian <gabionian7@gmail.com>
2022-11-14 22:18:13 +0800

    summation

--- resources/js/pages/Scoring/criteria/index.vue
+++ resources/js/pages/Scoring/criteria/index.vue
@@ -18,6 +18,7 @@
             <v-text-field
             :value="item.value"
             v-model="item.state"
+            
             >
                 
             </v-text-field>
@@ -25,16 +26,55 @@
 
             </v-col>
            </v-row>
-           <v-btn @click="click()">cc</v-btn>
+           <td> {{ totalScore }}</td>
+           <v-btn @click=" dialog = true">cc</v-btn>
 
            <v-text-field
-                v-model="add"
-               :value="sumField('value')"
+                id="totalordercost"
+                v-model="totalScore"
+                readonly
+                type="number"
            >
-
-           </v-text-field>
-            {{ this.item.value++}}
+           </v-text-field
+          
+           >
+           
         </v-card>
+        <v-dialog
+        v-model="dialog"
+        >
+            <v-card>
+                <v-data-table
+                    :headers="headers"
+                    :items="criteria"
+                    :items-per-page="5"
+                    class="elevation-1"
+                ></v-data-table>
+
+                
+               <!-- <table>
+                <tr>
+                    <td>Criteria</td>
+                    <td>Available point</td>
+                    <td>Given Point</td>
+                </tr>
+                <tr v-for="crit in criteria" :key="crit.id">
+                    <td>
+                        <input type="text" v-bind:data-id="crit.title" v-model="crit.title">
+                        <input type="text" v-bind:data-id="crit.point" v-model="crit.point">
+                        <input type="text" v-bind:data-id="crit.state" v-model="crit.state">
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        Total
+                    </td>
+                    <td> {{ totalScore }}</td>
+                </tr>
+               </table> -->
+
+            </v-card>
+        </v-dialog>
     </div>
 </template>
 <script>
@@ -45,40 +85,41 @@ import axios from '../../../plugins/axios'
                 criteria : [],
                 field: '',
                 sumup:'',
-                add: ''
+                add: '',
+                total : '',
+                dialog: false,
+                data : '',
+                headers : [
+                    {text : "Criteria", value: "title"},
+                    {text : "Available Point", value: "point"},
+                    {text : "Given Point", value: "state"},
+                ]
             }
         },
         mounted(){
             this.fetchCriteria()
         },
+        computed : {
+            totalScore(){
+                console.log('total score')
+                return this.criteria.reduce((acc, cur) => acc + Number(cur.state), 0);
+            }
+        },
+       
         methods: {
             fetchCriteria(){
                 axios.get('fetch-criteria/active/' + this.$route.params.id).then(response => {
                     this.criteria = response.data
                 });
             },
+           
+        }
+    }
 
-            click(){
-                this.criteria.reduce((a, b) => a + (b[key] || 0), 0)
-                this.sumup = sumField('value')
-            },
-
-             sumField(key) {
-                // sum data in give key (property)
-                return this.criteria.reduce((a, b) => a + (b[key] || 0), 0)
-            } 
+         
             
            
            
-        },
-        watch : {
-            // sumField(key) {
-            //     // sum data in give key (property)
-            //     return this.criteria.reduce((a, b) => a + (b[key] || 0), 0)
-            // } 
-          
-         
-        },
+       
         
-    }
 </script>
